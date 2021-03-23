dca18d1b452e96b5330e5609cdb7f39ac5ba5ff8
Suraj <surajprajapati8286@gmail.com>
2021-03-23 19:08:06 +0530

    Created 2 Fragments EditImageFragment and FiltersListFragment.

---
+++ app/src/main/java/com/dr/psycho/ryuk/EditImageFragment.java
@@ -0,0 +1,30 @@
+package com.dr.psycho.ryuk;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class EditImageFragment extends Fragment {
+
+
+    public EditImageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_image, container, false);
+    }
+}
