4cf81df15654ef1ab058fe71ed24cb87260a5597
Suraj <surajprajapati8286@gmail.com>
2021-03-24 12:33:43 +0530

    FiltersListFragment | EditImageFragment | MainActivity

--- app/src/main/java/com/dr/psycho/ryuk/FiltersListFragment.java
+++ app/src/main/java/com/dr/psycho/ryuk/FiltersListFragment.java
@@ -1,15 +1,42 @@
 package com.dr.psycho.ryuk;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.dr.psycho.ryuk.Adapter.ThumbnailAdapter;
+import com.dr.psycho.ryuk.Interface.FlitersListFragmentListener;
+import com.dr.psycho.ryuk.Utils.BitmapUtils;
+import com.dr.psycho.ryuk.Utils.SpacesItemDecoration;
+import com.zomato.photofilters.FilterPack;
+import com.zomato.photofilters.imageprocessors.Filter;
+import com.zomato.photofilters.utils.ThumbnailItem;
+import com.zomato.photofilters.utils.ThumbnailsManager;
 
-public class FiltersListFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FiltersListFragment extends Fragment implements  FlitersListFragmentListener{
+
+    RecyclerView recyclerView;
+    ThumbnailAdapter adapter;
+    List<ThumbnailItem> thumbnailItems;
+
+    FlitersListFragmentListener listener;
+
+    public void setListener(FlitersListFragmentListener listener) {
+        this.listener = listener;
+    }
 
     public FiltersListFragment() {
         // Required empty public constructor
@@ -24,6 +51,68 @@ public class FiltersListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filters_list, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_filters_list, container, false);
+        thumbnailItems = new ArrayList<>();
+        adapter = new ThumbnailAdapter(thumbnailItems, this, getActivity());
+
+        recyclerView = (RecyclerView)itemView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        int space = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8 , getResources().getDisplayMetrics());
+        recyclerView.addItemDecoration(new SpacesItemDecoration(space));
+        recyclerView.setAdapter(adapter);
+
+        displayThumbnail(null);
+        return itemView;
+    }
+
+    private void displayThumbnail(Bitmap bitmap)
+    {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Bitmap thumbImg;
+                if (bitmap == null)
+                    thumbImg = BitmapUtils.getBitmapFromAssets(getActivity(), MainActivity.pictureName, 100, 100);
+                else
+                    thumbImg = Bitmap.createScaledBitmap(bitmap , 100, 100, false);
+                if (thumbImg == null)
+                    return;
+                ThumbnailsManager.clearThumbs();
+                thumbnailItems.clear();
+
+                //add Normal Bitmap first
+                ThumbnailItem thumbnailItem =new ThumbnailItem();
+                thumbnailItem.image = thumbImg;
+                thumbnailItem.filterName = "Normal";
+                ThumbnailsManager.addThumb(thumbnailItem);
+
+                List<Filter> filters = FilterPack.getFilterPack(getActivity());
+
+                for (Filter filter : filters){
+                    ThumbnailItem tI = new ThumbnailItem();
+                    tI.image = thumbImg;
+                    tI.filter = filter;
+                    tI.filterName = filter.getName();
+                    ThumbnailsManager.addThumb(tI);
+                }
+
+                thumbnailItems.addAll(ThumbnailsManager.processThumbs(getActivity()));
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        };
+        new Thread(r).start();
+    }
+
+    @Override
+    public void onFilterSelected(Filter filter) {
+        if (listener != null)
+            listener.onFilterSelected(filter);
     }
 }
