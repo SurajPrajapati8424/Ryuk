63c06dd621bd4fd8b6f4f290ad5290f7b510f0eb
Suraj <surajprajapati8286@gmail.com>
2021-04-06 03:54:29 +0530

    Manual Code for hide Action and Title bar But done in Manifest file ^^

--- app/src/main/java/com/dr/psycho/ryuk/introActivity.java
+++ app/src/main/java/com/dr/psycho/ryuk/introActivity.java
@@ -1,6 +1,8 @@
 package com.dr.psycho.ryuk;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -18,7 +20,16 @@ public class introActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // make the activity on full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_intro);
+
+        // hide the action bar
+        getSupportActionBar().hide();
 
         // ini views
         tabLayout = findViewById(R.id.tab_indicator);
