4583e289587dd0a57bd0139d807f1dd2374864b2
Suraj <surajprajapati8286@gmail.com>
2021-04-06 03:53:14 +0530

    Added Indicator for ViewPager Guid Screen PHASE::02

--- app/src/main/java/com/dr/psycho/ryuk/introActivity.java
+++ app/src/main/java/com/dr/psycho/ryuk/introActivity.java
@@ -5,18 +5,23 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 
 public class introActivity extends AppCompatActivity {
 
     private ViewPager screenPager;
     IntroViewPagerAdapter introViewPagerAdapter;
-
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        // ini views
+        tabLayout = findViewById(R.id.tab_indicator);
 
         ArrayList<ScreenItem> mList = new ArrayList<>();
         mList.add(new ScreenItem("Click To Go","Start Editing From Fresh Clicked Image. Start Editing From Fresh Clicked Image \n Start Editing From Fresh Clicked Image",R.drawable.scooty));
@@ -27,6 +32,9 @@ public class introActivity extends AppCompatActivity {
         screenPager = findViewById(R.id.screenViewPager);
         introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
         screenPager.setAdapter(introViewPagerAdapter);
+
+        // setup tabLayout with viewPager
+        tabLayout.setupWithViewPager(screenPager);
 
     }
 
